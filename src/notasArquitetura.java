602a1bb1eb42dd5f8c0bfc8eec8e641ac76ec2ca
alejandro <droguelios@gmail.com>
2026-03-25 08:53:09 -0500

    se realizo parte de la historia usuario sobre la notacion de java

---
+++ src/notasArquitetura.java
@@ -0,0 +1,22 @@
+/* Notas de Arquitectura
+* Task 1: Evolucion del ecosistema Java
+*
+* ==========JAVA 8 (Legacy)============
+*
+*  Lanzado en 2014. Introdujo Lambdas, Streams y la API
+ * de Fecha/Hora. Fue un hito, pero su modelo de clases
+ * exige mucho "boilerplate": constructores, getters,
+ * setters, toString(), equals() y hashCode() escritos
+ * manualmente o generados por el IDE.
+ *
+ * Enfoque: IMPERATIVO / ORIENTADO A OBJETOS clásico.
+ * El desarrollador gestiona explícitamente el estado
+ * mutable de los objetos.
+ *
+ *
+ * */
+
+
+public class notasArquitetura {
+
+}
