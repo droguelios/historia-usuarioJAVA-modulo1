110409afbfa6566ca0c673607e24fe02ee92aef1
alejandro <droguelios@gmail.com>
2026-03-30 08:07:41 -0500

    Se realizo division o suma de bonos en salarios

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import Salario.Salarios;
 import empleados.Empleados;
 
 public class Main {
     public static void main(String[] args) {
         Empleados emp = new Empleados();
         emp.motrarInfo();
+
+        Salarios pme = new Salarios();
+        pme.actualizarBono(15.000);
 
 
     }
