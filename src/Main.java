fc1ea77949879409940ee6d91e3e429dad0833fc
alejandro <droguelios@gmail.com>
2026-03-30 08:48:04 -0500

    toda las tareas de la semana 1 completadas

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,23 @@ public class Main {
         emp.motrarInfo();
 
         Salarios pme = new Salarios();
-        pme.actualizarBono(15.000);
+        pme.actualizarBono(15000);
 
+        String nombre = null;
+        try{nombre.toUpperCase();
+
+
+        }catch (NullPointerException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        Empleados e1= new Empleados();
+        Empleados e2 = new Empleados();
+        System.out.println("e1 == e2: "+(e1 == e2));
+
+        Empleados e3 = e1;
+
+        System.out.println("e1 == e3" + (e1 == e3));
 
     }
 }
