dbb50c924889f0130d1d28bd179fa54b6a74f294
alejandro <droguelios@gmail.com>
2026-04-21 06:37:29 -0500

    feature: it make some files for historia_user 4 (late)

---
+++ corporate-talent-hub/src/main/java/org/riwi/user_history_4/DesempeñoReport.java
@@ -0,0 +1,4 @@
+package org.riwi.user_history_4;
+
+public record DesempeñoReport(int idempleado ,double promedio , String feedback) {
+}
---
+++ corporate-talent-hub/src/main/java/org/riwi/user_history_4/Persona.java
@@ -0,0 +1,13 @@
+package org.riwi.user_history_4;
+
+public abstract class Persona {
+
+    protected  String name ;
+    protected int age ;
+    public void respirar (){
+        System.out.println("respira bobotron");
+    }
+
+    public abstract void presentar();
+
+}
